3961a8264af4ea89d0a1883e73c54b461d1f1ddb
Xavier Verdejo <xavierverdejo@paucasesnovescifp.cat>
2020-10-15 20:52:00 +0200

    Updated solution of last Bonus Challenge. Updated and upgraded solution of last Bonus Challenge of ListModificationChallenge. Previous solution was hard-coded.

--- ListModificationChallenge/Program.cs
+++ ListModificationChallenge/Program.cs
@@ -207,12 +207,11 @@ namespace ListModificationChallenge
 
         private static void SortAList(List<PersonModel> people)
         {
-            List < PersonModel > tList = new List<PersonModel>();
+
             // TODO: Sort the incoming list values by fullname (ascending)
-            tList = people.OrderBy(x => x.FullName).ToList();
-            people.Clear();
-            people.AddRange(tList);
-            Console.WriteLine("DEBUG: " + people[1].LastName);
+            people.Sort((x, y) => string.Compare(x.FullName, y.FullName));
+            
+            //Console.WriteLine("DEBUG: " + people[1].LastName);
         }
         #endregion
 
