935d11e89801d60e28d145efbebfda90079ab89d
Xavier Verdejo <xavierverdejo@paucasesnovescifp.cat>
2020-10-16 19:45:02 +0200

    Passed DateTimeChallenge. Created a program that ask a Date to the user and compare it with actual date. Same with the time.

---
+++ DateTimeChallenge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            readDate();
+            readTime();
+            Console.ReadKey();
+        }
+        private static void readDate()
+        {
+            Console.WriteLine("Please introduce a date. ");
+            DateTime dateTime;
+            DateTime now = DateTime.Now;
+            if(DateTime.TryParse(Console.ReadLine(), out dateTime))
+            {
+                Console.WriteLine("Days between today and the input date: " + (int)((now - dateTime).TotalDays));
+            }
+            else
+            {
+                Console.WriteLine("Invalid date format");
+            }
+        }
+        private static void readTime()
+        {
+            Console.WriteLine("Please introduce a time. ");
+            DateTime dateTime;
+            DateTime now = DateTime.Now;
+            if (DateTime.TryParse(Console.ReadLine(), out dateTime))
+            {
+                Console.WriteLine("Hours between: " + (now.Hour - dateTime.Hour));
+                Console.WriteLine("Minutes between: " + (now.Minute - dateTime.Minute));
+            }
+            else
+            {
+                Console.WriteLine("Invalid date format");
+            }
+        }
+    }
+}
